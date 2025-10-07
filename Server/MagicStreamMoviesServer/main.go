474f4dbb54d9b4e4a4746d76fec5858b71116e4d
Jon Perry <jpper@umich.edu>
2025-10-07 00:35:53 -0400

    Import gin-gonic package and init server with go run

--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello, go world!")
+	// This is the main function
+
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, MagicStreamMovies")
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
+
 }
