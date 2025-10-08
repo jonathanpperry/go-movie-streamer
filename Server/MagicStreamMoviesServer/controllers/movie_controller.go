43d1503d2c7f790ac3e8149f0813cb596281d810
Jon Perry <jpper@umich.edu>
2025-10-07 23:24:52 -0400

    Created GetMovies gin-gonic endpoint handler function and mongodb connection code

---
+++ Server/MagicStreamMoviesServer/controllers/movie_controller.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/GavinLonDigital/MagicStreamMovies/Server/MagicStreamMoviesServer/database"
+	"github.com/GavinLonDigital/MagicStreamMovies/Server/MagicStreamMoviesServer/models"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
+
+var movieCollection *mongo.Collection = database.OpenCollection("movies")
+
+func GetMovies() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// c.JSON(200, gin.H{"message": "List of movies"})
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var movies []models.Movie
+
+		cursor, err := movieCollection.Find(ctx, bson.M{})
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": "Failed to fetch movies."})
+		}
+		defer cursor.Close((ctx))
+
+		if err = cursor.All(ctx, &movies); err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"Error": "Failed to decode movies."})
+		}
+
+		c.JSON(http.StatusOK, movies)
+	}
+}
--- Server/MagicStreamMoviesServer/main.go
+++ Server/MagicStreamMoviesServer/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+
+	controller "github.com/GavinLonDigital/MagicStreamMovies/Server/MagicStreamMoviesServer/controllers"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +15,8 @@ func main() {
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, MagicStreamMovies")
 	})
+
+	router.GET("/movies", controller.GetMovies())
 
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server", err)
