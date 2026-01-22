15927458a8c4674bb129abdf1088e9d128a5cb38
Jon Perry <jonperry113@gmail.com>
2026-01-22 13:58:58 -0500

    Create user model

---
+++ Server/MagicStreamMoviesServer/controllers/user_controller.go
@@ -0,0 +1,9 @@
+package controllers
+
+import (
+	"github.com/GavinLonDigital/MagicStreamMovies/Server/MagicStreamMoviesServer/models"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+)
